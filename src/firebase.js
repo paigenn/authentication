61e020e7ed7522863f8fbc78dd026ec1846b1507
paigenn <57846593+paigenn@users.noreply.github.com>
2020-01-06 17:22:05 -0700

    Connected to firebase (commit to master once it is up)
    Connected to firebase (commit to master once it is up)

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBNc9V27Sc2UAJLY2G2dDJ5If-ORNaoj9Q",
+  authDomain: "authentication-84eb7.firebaseapp.com",
+  databaseURL: "https://authentication-84eb7.firebaseio.com",
+  projectId: "authentication-84eb7",
+  storageBucket: "authentication-84eb7.appspot.com",
+  messagingSenderId: "896033014545",
+  appId: "1:896033014545:web:87d7744ce4222fb43202b0",
+  measurementId: "G-KQL5PEGSMF"
 };
 
 firebase.initializeApp(config);
